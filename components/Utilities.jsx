b4f08ae5b527bb3e686a9babc6d80837a3f60894
GunixYT <gammerpro06@gmail.com>
2023-06-12 11:01:42 -0500

    Update Utilities.jsx

--- components/Utilities.jsx
+++ components/Utilities.jsx
@@ -2,7 +2,7 @@ import { BsGithub, BsTwitter, BsTwitch } from "react-icons/bs";
 
 export const Redes = () => {
   return (
-    <div className='absolute bottom-0 right-0 mr-6 mb-2 z-50 2xl:text-lg xl:text-base lg:text-sm md:text-xs'>
+    <div className='absolute bottom-0 right-0 mr-6 mb-2 z-50 2xl:text-lg xl:text-base lg:text-sm md:text-xs sm:text-xs'>
       <div>
         <h1>Programador:</h1>
         <a className='relative' href='https://twitter.com/GuniX41_' target='_blank' rel='noopener noreferrer'>
